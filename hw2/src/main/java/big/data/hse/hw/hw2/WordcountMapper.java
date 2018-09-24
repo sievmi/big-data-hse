207a5578862a42d53dab02dd670104c42c2c4c78
sievmi <sievmi@yandex-team.ru>
2018-09-24 18:26:16 +0300

    experiment with title [2]

--- hw2/src/main/java/big/data/hse/hw/hw2/WordcountMapper.java
+++ hw2/src/main/java/big/data/hse/hw/hw2/WordcountMapper.java
@@ -23,9 +23,11 @@ public class WordcountMapper extends Mapper<LongWritable, Text, Text, IntWritabl
             String[] words = splitted[2].toLowerCase().split("[^\\p{L}]");
 
             IntWritable textLength = new IntWritable(words.length);
-            for (String word : words) {
-                context.write(new Text(title), textLength);
-            }
+            /*for (String word : words) {
+                context.write(new Text(word), textLength);
+            }*/
+
+            context.write(new Text(title), textLength);
         }
     }
 }
