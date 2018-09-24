c0fb0dede10745238e3c4e55b67c8e7384010380
sievmi <sievmi@yandex-team.ru>
2018-09-24 15:32:41 +0300

    fix sorting mapper

--- hw2/src/main/java/big/data/hse/hw/hw2/SortingMapper.java
+++ hw2/src/main/java/big/data/hse/hw/hw2/SortingMapper.java
@@ -1,6 +1,7 @@
 package big.data.hse.hw.hw2;
 
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -10,10 +11,14 @@ import java.io.IOException;
  * Created by sievmi on 23.09.18
  */
 
-public class SortingMapper extends Mapper<Text, DoubleWritable, Text, DoubleWritable> {
+public class SortingMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
     @Override
-    protected void map(Text key, DoubleWritable value, Context context)
+    protected void map(LongWritable offset, Text text, Context context)
             throws IOException, InterruptedException {
-        context.write(new Text(String.format(value.toString(), "%10.2f") + key.toString()), value);
+        String[] splitted = text.toString().split("\t");
+        String value = splitted[0];
+        String word = splitted[1];
+        context.write(new Text(String.format(value, "%10.2f") + word),
+                new DoubleWritable(Double.parseDouble(value)));
     }
 }
