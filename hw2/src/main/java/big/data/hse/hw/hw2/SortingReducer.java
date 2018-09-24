3cf30db111f1e8debd518d49072905224c31a5b5
sievmi <sievmi@yandex-team.ru>
2018-09-24 16:09:32 +0300

    added sorting reducer [2]

---
+++ hw2/src/main/java/big/data/hse/hw/hw2/SortingReducer.java
@@ -0,0 +1,26 @@
+package big.data.hse.hw.hw2;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * Created by sievmi on 24.09.18
+ */
+
+public class SortingReducer extends Reducer<Text, DoubleWritable, Text, Text> {
+
+    @Override
+    protected void reduce(Text key, Iterable<DoubleWritable> values, Context context)
+            throws IOException, InterruptedException {
+
+        String[] splitted = key.toString().split(" ");
+        String value = splitted[0].trim();
+        String word = splitted[1];
+
+        context.write(new Text(word), new Text(word + "\t" + value));
+    }
+
+}
