aaa6ddfd0285946565e31a186fe13bfe03c3f8bd
sievmi <sievmi@yandex-team.ru>
2018-10-09 20:22:16 +0300

    add caluclate disp

--- hw3/src/main/java/big/data/hse/hw/hw3/AverageReducer.java
+++ hw3/src/main/java/big/data/hse/hw/hw3/AverageReducer.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by sievmi on 09.10.18
@@ -18,6 +19,8 @@ public class AverageReducer extends Reducer<IntWritable, Text, IntWritable, Text
         // match_id player_name x y
 
         int placement = key.get();
+        ArrayList<Double> arrX = new ArrayList<Double>();
+        ArrayList<Double> arrY = new ArrayList<Double>();
 
         int cnt = 0;
         double sumX = 0;
@@ -26,16 +29,36 @@ public class AverageReducer extends Reducer<IntWritable, Text, IntWritable, Text
             cnt += 1;
             String[] splitted = value.toString().trim().split("\t");
             double x = Double.parseDouble(splitted[0]);
+            arrX.add(x);
             double y = Double.parseDouble(splitted[1]);
+            arrY.add(y);
 
             sumX += x;
             sumY += y;
         }
-        String averageStrX = String.format("%10.2f", sumX / cnt);
-        String averageStrY = String.format("%10.2f", sumY / cnt);
+
+        double averageX = sumX / cnt;
+        String averageStrX = String.format("%10.2f", averageX);
+        double averageY = sumY / cnt;
+        String averageStrY = String.format("%10.2f", averageY);
+
+        double dX = 0;
+        for (Double x: arrX) {
+            dX += (x - averageX) * (x - averageX) / (cnt - 1);
+        }
+        dX = Math.sqrt(dX);
+        String dStrX = String.format("%10.2f", dX);
+
+        double dY = 0;
+        for (Double y: arrY) {
+            dX += (y - averageY) * (y - averageY) / (cnt - 1);
+        }
+        dY = Math.sqrt(dY);
+        String dStrY = String.format("%10.2f", dY);
 
 
-        context.write(key, new Text(averageStrX + "\t" + averageStrY));
+
+        context.write(key, new Text(averageStrX + "\t" + averageStrY + "\t" + dStrX + "\t" + dStrY));
     }
 
 }
