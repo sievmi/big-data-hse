85ad8430e115b61ff67f6e7d5acdc8a3d3778304
sievmi <sievmi@yandex-team.ru>
2018-11-13 23:54:01 +0300

    task3

---
+++ hw5/src/main/scala/task3/StopWordsCount.scala
@@ -0,0 +1,40 @@
+package task3
+
+import java.io.PrintWriter
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by sievmi on 13.11.18  
+  */
+object StopWordsCount {
+  def main(args: Array[String]) {
+
+    val conf: SparkConf = new SparkConf().setAppName("Count capital letters").setMaster("yarn")
+    val sc: SparkContext = new SparkContext(conf)
+
+    val input = sc.textFile("/data/wiki/en/articles")
+
+    val stopWordsAccums = Map("and" -> sc.longAccumulator("and accumulator"), "the" -> sc.longAccumulator("the accumulator"))
+
+    val wordsRDD = input.flatMap(line ⇒ line.split("\t").tail)
+      .flatMap(_.split(" "))
+
+    wordsRDD.map(word => {
+      if (stopWordsAccums.contains(word)) {
+        stopWordsAccums(word).add(1)
+      }
+    })
+
+    val fs = FileSystem.get(new Configuration())
+    val outputWriter = new PrintWriter(fs.create(new Path("/user/esidorov/hw5/task3")))
+    outputWriter.write("and - ")
+    outputWriter.write(s"${stopWordsAccums("end").value}")
+    outputWriter.write("the - ")
+    outputWriter.write(s"${stopWordsAccums("the").value}")
+    outputWriter.flush()
+    outputWriter.close()
+  }
+}
