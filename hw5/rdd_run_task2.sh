5cd98de2fa43e4b88aa329128167c2b3d7d44f35
sievmi <sievmi@yandex-team.ru>
2018-11-16 18:27:30 +0300

    rdd task2

--- hw5/rdd_run_task2.sh
+++ hw5/rdd_run_task2.sh
@@ -2,6 +2,6 @@
 
 hdfs dfs -rm -r hw5/rdd/task2
 
-spark2-submit --class task2.rdd.TopBigramm ./target/scala-2.11/hw5_2.11-0.1.jar
+spark2-submit --class rdd.task2.TopBigramm ./target/scala-2.11/hw5_2.11-0.1.jar
 
 hdfs dfs -cat hw5/rdd/task2
