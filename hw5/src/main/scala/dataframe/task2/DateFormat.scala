00dc2eb3788a1212f42491dd49980867a571b9c7
sievmi <sievmi@yandex-team.ru>
2018-11-16 16:36:46 +0300

    final task2

--- hw5/src/main/scala/dataframe/task2/DateFormat.scala
+++ hw5/src/main/scala/dataframe/task2/DateFormat.scala
@@ -24,7 +24,7 @@ object DateFormat {
       .csv("/user/pakhtyamov/data/user_logs/user_logs_M/logsLM.txt")
 
     val formattedDF = inputDF.select("ip", "date", "url", "c5", "status", "browser")
-      .withColumn("date", to_date(col("date"), "yyyyMMddHHmmss"))
+      .withColumn("date", date_format(to_date(col("date"), "yyyyMMddHHmmss"), "yyMMdddd HHmm"))
 
     formattedDF.rdd.saveAsTextFile("/user/esidorov/hw5/dataframes/task2")
   }
