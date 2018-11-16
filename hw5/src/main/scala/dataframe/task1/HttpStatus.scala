7405c9ad22b8f638d85b76acbf55e0969b5f6912
sievmi <sievmi@yandex-team.ru>
2018-11-16 15:41:58 +0300

    final task1 df

--- hw5/src/main/scala/dataframe/task1/HttpStatus.scala
+++ hw5/src/main/scala/dataframe/task1/HttpStatus.scala
@@ -22,26 +22,20 @@ object HttpStatus {
     val inputDF = sqlContext.read.option("sep", "\t").schema(schema)
       .csv("/user/pakhtyamov/data/user_logs/user_logs_M/logsLM.txt")
 
-    val selected = inputDF.select("status", "ip")
-    val filteredSelected = selected.filter(!_.anyNull)
-    val ansDfDistinct = filteredSelected.groupBy("status").agg(countDistinct("ip")).as("count")
-    val ans = filteredSelected.groupBy("status").agg(count("ip")).as("count")
+    val ansDF = inputDF.select("status", "ip").filter(!_.anyNull)
+      .groupBy("status").agg(countDistinct("ip")).as("count")
 
     val fs = FileSystem.get(new Configuration())
     val outputWriter = new PrintWriter(fs.create(new Path("/user/esidorov/hw5/dataframes/task1")))
-    outputWriter.println(s"Row size = ${inputDF.collect().length}")
-    outputWriter.println(s"Selected size = ${selected.collect().length}")
-    outputWriter.println(s"Selected filtered size = ${filteredSelected.collect().length}")
-    outputWriter.println(s"Ans distinct size = ${ansDfDistinct.collect().length}")
-    outputWriter.println(s"Ans size = ${ans.collect().size}")
 
     outputWriter.println(inputDF.columns.mkString(","))
     outputWriter.println()
-    ansDfDistinct.collect().foreach(outputWriter.println)
+    ansDF.collect().foreach(outputWriter.println)
     outputWriter.flush()
     outputWriter.close()
   }
 
-  case class UserLog(ip: String, c1: String, c2: String, c3: String, url: String, c5: String, status: String, browser: String)
+  case class UserLog(ip: String, c1: String, c2: String, c3: String, url: String,
+                     c5: String, status: String, browser: String)
 
 }
