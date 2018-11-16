7164506b4c0c87d00c81691c6403b67e37c2ae8b
sievmi <sievmi@yandex-team.ru>
2018-11-16 15:30:20 +0300

    a

--- hw5/src/main/scala/dataframe/task1/HttpStatus.scala
+++ hw5/src/main/scala/dataframe/task1/HttpStatus.scala
@@ -6,7 +6,7 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{Encoders, SQLContext, SparkSession}
-import org.apache.spark.sql.functions.countDistinct
+import org.apache.spark.sql.functions.{countDistinct, count}
 
 /**
   * Created by sievmi on 16.11.18  
@@ -22,13 +22,21 @@ object HttpStatus {
     val inputDF = sqlContext.read.option("sep", "\t").schema(schema)
       .csv("/user/pakhtyamov/data/user_logs/user_logs_M/logsLM.txt")
 
-    val selected = inputDF.select("status", "ip").filter(!_.anyNull).groupBy("status").agg(countDistinct("ip")).as("count")
+    val selected = inputDF.select("status", "ip")
+    val filteredSelected = selected.filter(!_.anyNull)
+    val ansDfDistinct = filteredSelected.groupBy("status").agg(countDistinct("ip")).as("count")
+    val ans = filteredSelected.groupBy("status").agg(count("ip")).as("count")
 
     val fs = FileSystem.get(new Configuration())
     val outputWriter = new PrintWriter(fs.create(new Path("/user/esidorov/hw5/dataframes/task1")))
+    outputWriter.println(s"Row size = ${inputDF.collect().length}")
+    outputWriter.println(s"Selected size = ${selected.collect().length}")
+    outputWriter.println(s"Ans distinct size = ${ansDfDistinct.collect().length}")
+    outputWriter.println(s"Ans size = ${ans.collect().size}")
+
     outputWriter.println(inputDF.columns.mkString(","))
     outputWriter.println()
-    selected.collect().foreach(outputWriter.println)
+    ansDfDistinct.collect().foreach(outputWriter.println)
     outputWriter.flush()
     outputWriter.close()
   }
