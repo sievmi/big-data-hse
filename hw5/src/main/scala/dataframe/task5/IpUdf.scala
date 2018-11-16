b92dfe93e9b4a94d2fcf09ab8b43b6c1b120138f
sievmi <sievmi@yandex-team.ru>
2018-11-16 17:36:32 +0300

    a

--- hw5/src/main/scala/dataframe/task5/IpUdf.scala
+++ hw5/src/main/scala/dataframe/task5/IpUdf.scala
@@ -5,8 +5,9 @@ import java.io.PrintWriter
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{Encoders, SparkSession}
-import org.apache.spark.sql.functions.{udf, col}
+import org.apache.spark.sql.{Encoders, Row, SparkSession}
+import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
 
 /**
   * Created by sievmi on 16.11.18  
@@ -18,15 +19,23 @@ object IpUdf {
     val sc: SparkContext = new SparkContext(conf)
     val sqlContext = SparkSession.builder().getOrCreate().sqlContext
 
-    val splitIp = (ip: String) => ip.split(".").map(_.toInt).mkString(" ")
+    val splitIp = (ip: String) => ip.split('.').map(_.toInt).mkString(" ")
     val splitUdf = udf(splitIp)
 
     val userSchema = Encoders.product[User].schema
     val usersDF = sqlContext.read.option("sep", "\t").schema(userSchema)
       .csv("/user/pakhtyamov/data/user_logs/user_data_M/userDataM")
-      .withColumn("splitted", splitUdf(col("ip")))
 
-    usersDF.rdd.saveAsTextFile("/user/esidorov/hw5/dataframes/task5")
+
+    val usersRDD = usersDF.withColumn("splitted", splitUdf(col("ip"))).rdd
+    val newSchema = StructType(usersDF.schema.fields.init ++
+      Array(StructField("ip_1", IntegerType), StructField("ip_2", IntegerType),
+        StructField("ip_3", IntegerType), StructField("ip_4", IntegerType)))
+    val newRDD = usersRDD.map(r => Row.fromSeq(
+      r.toSeq.init ++ r.getAs[String]("splitted").split(' ')))
+    sqlContext.createDataFrame(newRDD, newSchema)
+
+    newRDD.saveAsTextFile("/user/esidorov/hw5/dataframes/task5")
   }
 
   case class UserLog(ip: String, c1: String, c2: String, c3: String, url: String,
